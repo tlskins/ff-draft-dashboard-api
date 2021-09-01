9145d5d5e8774a361feda3a73fecfe8cd48e3bfb
Tim Lee <tlee87@gmail.com>
2021-09-01 09:25:14 -0400

    move stuff

--- parsers/espn_parser.go
+++ parsers/espn_parser.go
@@ -63,45 +63,6 @@ func HttpRequest(client *http.Client, method, url string, headers map[string][]s
 	return nil
 }
 
-func HttpHtmlRequest(client *http.Client, method, url string, headers map[string][]string, data interface{}) (out string, err error) {
-	var body io.Reader
-	if data != nil {
-		if b, ok := data.([]byte); ok {
-			body = bytes.NewReader(b)
-		} else {
-			if b, jErr := json.Marshal(data); jErr == nil {
-				body = bytes.NewReader(b)
-			} else {
-				err = errors.New(fmt.Sprintf("Error marshaling request data: %v", jErr.Error()))
-				return
-			}
-		}
-	}
-
-	req, hErr := http.NewRequest(method, url, body)
-	if hErr != nil {
-		err = errors.New(fmt.Sprintf("Error building http request: %v", hErr.Error()))
-		return
-	}
-	for header, values := range headers {
-		req.Header[header] = values
-	}
-
-	resp, cErr := client.Do(req)
-	if cErr != nil {
-		err = errors.New(fmt.Sprintf("Error sending http request: %v", cErr.Error()))
-		return
-	}
-
-	b, iErr := ioutil.ReadAll(resp.Body)
-	if iErr != nil {
-		err = errors.New(fmt.Sprintf("Error reading response body: %v", iErr.Error()))
-		return
-	}
-	out = string(b)
-	return
-}
-
 const EspnApiUrl = "https://fantasy.espn.com/apis/v3/games/ffl/seasons/2021/segments/0/leaguedefaults/3?view=kona_player_info"
 
 func EspnQueryHeader(limit, offset int) (out map[string][]string) {
--- parsers/fpros_parser.go
+++ parsers/fpros_parser.go
@@ -1,3 +1,53 @@
 package parsers
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+func HttpHtmlRequest(client *http.Client, method, url string, headers map[string][]string, data interface{}) (out string, err error) {
+	var body io.Reader
+	if data != nil {
+		if b, ok := data.([]byte); ok {
+			body = bytes.NewReader(b)
+		} else {
+			if b, jErr := json.Marshal(data); jErr == nil {
+				body = bytes.NewReader(b)
+			} else {
+				err = errors.New(fmt.Sprintf("Error marshaling request data: %v", jErr.Error()))
+				return
+			}
+		}
+	}
+
+	req, hErr := http.NewRequest(method, url, body)
+	if hErr != nil {
+		err = errors.New(fmt.Sprintf("Error building http request: %v", hErr.Error()))
+		return
+	}
+	for header, values := range headers {
+		req.Header[header] = values
+	}
+
+	resp, cErr := client.Do(req)
+	if cErr != nil {
+		err = errors.New(fmt.Sprintf("Error sending http request: %v", cErr.Error()))
+		return
+	}
+
+	b, iErr := ioutil.ReadAll(resp.Body)
+	if iErr != nil {
+		err = errors.New(fmt.Sprintf("Error reading response body: %v", iErr.Error()))
+		return
+	}
+	out = string(b)
+	return
+}
+
 const FProsApiUrl = "https://www.fantasypros.com/nfl/rankings/ppr.php"
